77f857069b5f702727a81c0be029ea1e57a431f0
piechu007 <534405691+piechu007@users.noreply.github.com>
2020-02-01 10:46:11 +0100

    Działa Strzałka celu

--- Assets/Scripts/quests/Arrow.cs
+++ Assets/Scripts/quests/Arrow.cs
@@ -2,17 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class arrow : MonoBehaviour
+public class Arrow : MonoBehaviour
 {
+    [SerializeField]
+    Transform target;
+    [SerializeField]
+    Transform ship;
+    [SerializeField]
+    float distanceFromeBorder = 0.1f;
 
+    Vector3 startPozycion;
+    float screenX;
+    float screenY;
+    RectTransform rectTransform;
 
     void Start()
     {
-        
+        rectTransform = GetComponent<RectTransform>();
+        startPozycion = rectTransform.position;
+        screenX = (Screen.width/2) * (1-distanceFromeBorder);
+        screenY = (Screen.height/2) * (1-distanceFromeBorder);
     }
 
     void Update()
     {
-        
+        Vector3 direction = Direction();
+        //rectTransform.position = direction + startPozycion;
+        if(Mathf.Abs(direction.x/direction.y) > screenX/screenY)
+        {
+            direction = direction * (screenX / Mathf.Abs(direction.x));
+        }
+        else
+        {
+            direction = direction * (screenY / Mathf.Abs(direction.y));
+        }
+        rectTransform.position = direction + startPozycion;
+        rectTransform.eulerAngles = Vector3.forward * Vector3.SignedAngle(direction, Vector3.up, Vector3.back);
+    }
+
+    Vector3 Direction()
+    {
+        return target.position - ship.position;//+ new Vector3(600,314,0);
     }
 }
