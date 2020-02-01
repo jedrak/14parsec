21f21cef41d246716abe028aa168d4b2980e4fcb
Andrzej Zabski <jedrak32@gmail.com>
2020-02-02 00:29:43 +0100

    cake prefab

---
+++ Assets/Scripts/Ship/Modules/Cake.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cake : MonoBehaviour, IAttachable
+{
+    public void OnAttach()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnDettach()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
