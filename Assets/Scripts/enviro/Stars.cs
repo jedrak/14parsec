87a2a11b2123b922c1d08453c8e637a7e1f7c829
piechu007 <534405691+piechu007@users.noreply.github.com>
2020-02-02 13:41:52 +0100

    Strzałk jest biała a gwiazdy się bagują

---
+++ Assets/Scripts/enviro/Stars.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stars : MonoBehaviour
+{
+    StarsControler controler;
+    int wiersz, kolumna;
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<ShipMove>() != null)
+        {
+            Vector3 direction = collision.transform.position - transform.position;
+            float angle = Vector3.SignedAngle(direction, Vector3.up, Vector3.back);
+
+            if (angle > 45 && angle < 135)          controler.Move(wiersz, kolumna, Vector2.left);
+            else if (angle < -45 && angle > -135)   controler.Move(wiersz, kolumna, Vector2.right);
+            else if (angle > -45 && angle < 45)     controler.Move(wiersz, kolumna, Vector2.up);
+            else if (angle > 135 || angle < -135)   controler.Move(wiersz, kolumna, Vector2.down);
+        }
+    }
+
+
+
+    public void SetParameters(StarsControler starsControler,int i, int j)
+    {
+        controler = starsControler;
+        wiersz = i;
+        kolumna = j;
+    }
+}
---
+++ Assets/Scripts/enviro/StarsControler.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarsControler : MonoBehaviour
+{
+    [SerializeField]
+    GameObject starsOBJ;
+
+    Stars[][] stars;
+
+    void Start()
+    {
+        stars = new Stars[3][];
+        for (int i=0; i<3; i++)
+        {
+            stars[i] = new Stars[3];
+            for (int j = 0; j < 3; j++)
+            {
+                GameObject pom = Instantiate(starsOBJ,new Vector3((j-1)*50, (i-1)*50, 0), Quaternion.identity);
+                stars[i][j] = pom.GetComponent<Stars>();
+                stars[i][j].SetParameters(this,i,j);
+            }
+        }
+    }
+
+    public void Move(int wiersz, int kolumna, Vector2 v)
+    {
+        if(v.x == 0)
+        {
+            if (v.y == -1)
+            {
+                wiersz = (wiersz + 1) % 3;
+                Debug.Log("gora");
+            }
+            else
+            {
+                wiersz = (wiersz + 4) % 3;
+                Debug.Log("dol");
+            }
+            for(int i=0;i<3;i++)
+            {
+                stars[wiersz][i].transform.position += (Vector3)v * (-150);
+            }
+        }
+        else
+        {
+            if (v.x == -1)
+            {
+                kolumna = (kolumna + 1) % 3;
+                Debug.Log("prawo");
+            }
+            else
+            {
+                kolumna = (kolumna + 4) % 3;
+                Debug.Log("lewo");
+            }
+            for (int j = 0; j < 3; j++)
+            {
+                stars[j][kolumna].transform.position += (Vector3)v * (-150);
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
